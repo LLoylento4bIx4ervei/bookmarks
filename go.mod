08863d431ff1d6c9fbe682ecf36fec74c3e562e2
LLoylento4bIx4ervei <68648015+LLoylento4bIx4ervei@users.noreply.github.com>
2025-01-19 19:45:43 +0300

    finish

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LLoylento4bIx4ervei/bookmarks
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	bookmarks := map[int]string{}
+	realizeMenu(bookmarks)
+}
+
+func menu() int {
+	fmt.Println("Меню")
+	fmt.Println("1. Посмотреть закладку")
+	fmt.Println("2. Добавить закладку")
+	fmt.Println("3. Удалить закладку")
+	fmt.Println("4. Выход")
+	var userAnswer int
+	fmt.Scan(&userAnswer)
+	return userAnswer
+}
+
+func seeBookmarks(bookmarks map[int]string) {
+	if len(bookmarks) == 0 {
+		fmt.Println("Список закладок пуст")
+		return
+	}
+	for k, v := range bookmarks {
+		fmt.Printf("Ключ: %v, Значение: %v\n", k, v)
+	}
+}
+
+func addBookmarks(bookmarks map[int]string) map[int]string {
+
+	var bokKey int
+	fmt.Println("Введите ключ: ")
+	fmt.Scan(&bokKey)
+	var bokVal string
+	fmt.Println("Введите значение: ")
+	fmt.Scan(&bokVal)
+	bookmarks[bokKey] = bokVal
+	return bookmarks
+}
+
+func delBookmarks(bookmarks map[int]string) map[int]string {
+
+	var delKey int
+	fmt.Println("Ведите ключ для удаления закладки: ")
+	fmt.Scan(&delKey)
+	delete(bookmarks, delKey)
+	return bookmarks
+}
+
+func realizeMenu(bookmarks map[int]string) {
+	for {
+		fmt.Println("Закладки")
+		answer := menu()
+		switch answer {
+
+		case 1:
+			seeBookmarks(bookmarks)
+		case 2:
+			addBookmarks(bookmarks)
+		case 3:
+			delBookmarks(bookmarks)
+		case 4:
+			return
+		}
+	}
+}
